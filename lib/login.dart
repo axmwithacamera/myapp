679833072258abe507ecb6227bd8660a80d7d0fa
axmwithacamera <110785644+axmwithacamera@users.noreply.github.com>
2023-05-26 09:48:33 +0500

    signup
    in this part, if any field is left empty, app would not go to home.dart. instead, it shows a pop up message asking the user to fill all fields

--- lib/login.dart
+++ lib/login.dart
@@ -105,7 +105,7 @@ class _LoginState extends State<Login> {
             TextButton(
               onPressed: () {
                 Navigator.push(
-                    context, MaterialPageRoute(builder: (_) => const Signup()));
+                    context, MaterialPageRoute(builder: (_) => Signup()));
               },
               child: const Text(
                 'New User? Create Account',
--- lib/signup.dart
+++ lib/signup.dart
@@ -3,31 +3,24 @@ import 'package:myapp/home.dart';
 import 'login.dart';
 
 class Signup extends StatefulWidget {
-  const Signup({super.key});
+  Signup({Key? key}) : super(key: key);
 
   @override
   State<Signup> createState() => _SignupState();
 }
 
 class _SignupState extends State<Signup> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController phoneNumberController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //     appBar: AppBar(
-      //   title: const Text("Signup"),
-      //   centerTitle: true,
-      //   backgroundColor: Colors.amber,
-      // )
-
       backgroundColor: const Color.fromRGBO(5, 100, 92, 1),
-      // appBar: AppBar(
-      //   backgroundColor: Colors.black,
-      //   title: const Text(
-      //     'AppBar',
-      //     style: TextStyle(color: Colors.white),
-      //   ),
-      // ),
-      // body: Column(children: [Text("what"), col_2(), hhhhghh()]),
       body: Container(
         padding: const EdgeInsets.all(30),
         width: double.maxFinite,
@@ -39,114 +32,127 @@ class _SignupState extends State<Signup> {
             colors: [
               Color.fromARGB(255, 248, 248, 248),
               Color.fromARGB(255, 155, 255, 252),
-              Color.fromARGB(255, 84, 221, 255)
+              Color.fromARGB(255, 84, 221, 255),
             ],
           ),
         ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            //Name field
-            const Padding(
-              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
-              padding: EdgeInsets.symmetric(horizontal: 15),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
               child: TextField(
+                controller: nameController,
                 decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Name',
-                    hintText: 'Enter your name eg: Ahmed Mohamed',
-                    hintStyle: TextStyle(color: Colors.black45)),
+                  border: OutlineInputBorder(),
+                  labelText: 'Name',
+                  hintText: 'Enter your name eg: Ahmed Mohamed',
+                  hintStyle: TextStyle(color: Colors.black45),
+                ),
               ),
             ),
-
-            const Padding(
-              padding:
-                  EdgeInsets.only(left: 15.0, right: 15.0, top: 10, bottom: 10),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 15.0, right: 15.0, top: 10, bottom: 10),
             ),
-            //Phone number field
-            const Padding(
-              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
-              padding: EdgeInsets.symmetric(horizontal: 15),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
               child: TextField(
+                controller: phoneNumberController,
                 decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Phone Number',
-                    hintText: 'Enter your mobile number',
-                    hintStyle: TextStyle(color: Colors.black45)),
+                  border: OutlineInputBorder(),
+                  labelText: 'Phone Number',
+                  hintText: 'Enter your mobile number',
+                  hintStyle: TextStyle(color: Colors.black45),
+                ),
               ),
             ),
-
-            const Padding(
-              padding:
-                  EdgeInsets.only(left: 15.0, right: 15.0, top: 10, bottom: 10),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 15.0, right: 15.0, top: 10, bottom: 10),
             ),
-
-            //email field
-            const Padding(
-              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
-              padding: EdgeInsets.symmetric(horizontal: 15),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
               child: TextField(
+                controller: emailController,
                 decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Email',
-                    hintText: 'Enter valid email id as abc@gmail.com',
-                    hintStyle: TextStyle(color: Colors.black45)),
+                  border: OutlineInputBorder(),
+                  labelText: 'Email',
+                  hintText: 'Enter valid email id as abc@gmail.com',
+                  hintStyle: TextStyle(color: Colors.black45),
+                ),
               ),
             ),
-
-            const Padding(
-              padding:
-                  EdgeInsets.only(left: 15.0, right: 15.0, top: 10, bottom: 10),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 15.0, right: 15.0, top: 10, bottom: 10),
             ),
-
-            //email field
-            const Padding(
-              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
-              padding: EdgeInsets.symmetric(horizontal: 15),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
               child: TextField(
+                controller: passwordController,
                 decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                    hintText: 'Enter your password',
-                    hintStyle: TextStyle(color: Colors.black45)),
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                  hintText: 'Enter your password',
+                  hintStyle: TextStyle(color: Colors.black45),
+                ),
               ),
             ),
-
-            const Padding(
-              padding:
-                  EdgeInsets.only(left: 15.0, right: 15.0, top: 10, bottom: 10),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 15.0, right: 15.0, top: 10, bottom: 10),
             ),
-
-            //Password field
-            const Padding(
-              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
-              padding: EdgeInsets.symmetric(horizontal: 15),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
               child: TextField(
+                controller: confirmPasswordController,
                 decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Confirm Password',
-                    hintText: 'Enter your password',
-                    hintStyle: TextStyle(color: Colors.black45)),
+                  border: OutlineInputBorder(),
+                  labelText: 'Confirm Password',
+                  hintText: 'Enter your password',
+                  hintStyle: TextStyle(color: Colors.black45),
+                ),
               ),
             ),
-
-            const Padding(
-              padding:
-                  EdgeInsets.only(left: 15.0, right: 15.0, top: 30, bottom: 30),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 15.0, right: 15.0, top: 30, bottom: 30),
             ),
-
             Container(
               height: 40,
               width: 200,
               decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 2, 91, 104),
-                  borderRadius: BorderRadius.circular(20)),
+                color: const Color.fromARGB(255, 2, 91, 104),
+                borderRadius: BorderRadius.circular(20),
+              ),
               child: TextButton(
                 onPressed: () {
-                  setState(() {});
-                  Navigator.push(
-                      context, MaterialPageRoute(builder: (_) => const Home()));
-                  debugPrint("pressed Sign Up");
+                  if (_validateFields()) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (_) => const Home()),
+                    );
+                    debugPrint("pressed Sign Up");
+                  } else {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text('Error'),
+                          content: Text('Please fill all the fields.'),
+                          actions: <Widget>[
+                            TextButton(
+                              child: Text('OK'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  }
                 },
                 child: const Text(
                   'Sign Up',
@@ -157,7 +163,9 @@ class _SignupState extends State<Signup> {
             TextButton(
               onPressed: () {
                 Navigator.push(
-                    context, MaterialPageRoute(builder: (_) => const Login()));
+                  context,
+                  MaterialPageRoute(builder: (_) => const Login()),
+                );
                 debugPrint("pressed Back to login");
               },
               child: const Text(
@@ -170,5 +178,13 @@ class _SignupState extends State<Signup> {
         ),
       ),
     );
+  }
+
+  bool _validateFields() {
+    return nameController.text.isNotEmpty &&
+        phoneNumberController.text.isNotEmpty &&
+        emailController.text.isNotEmpty &&
+        passwordController.text.isNotEmpty &&
+        confirmPasswordController.text.isNotEmpty;
   }
 }
